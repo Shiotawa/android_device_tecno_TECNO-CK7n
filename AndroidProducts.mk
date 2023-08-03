c68ebe863abc3c7987faf0cacd81d95d482f8280
Shiotawa | #BreakTheCode <61047914+Shiotawa@users.noreply.github.com>
2023-08-03 12:23:56 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_TECNO-CK7n.mk
+    $(LOCAL_DIR)/twrp_CK7n.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_TECNO-CK7n-user \
-    twrp_TECNO-CK7n-userdebug \
-    twrp_TECNO-CK7n-eng
+    twrp_CK7n-user \
+    twrp_CK7n-userdebug \
+    twrp_CK7n-eng
