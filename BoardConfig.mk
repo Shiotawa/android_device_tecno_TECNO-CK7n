6384b2e839996a7ce8ae7a7a6f57bdf919d1ad7e
Shiotawa | #BreakTheCode <61047914+Shiotawa@users.noreply.github.com>
2023-08-05 23:00:32 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/TECNO-CK7n
+DEVICE_PATH := device/tecno/CK7n
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
